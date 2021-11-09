cf3f7bc3163cd16fdc15ba3af3cf3570cd4c92e8
l1dl1d <javeekh@ssfs.org>
2021-11-09 09:55:52 -0500

    finished printing upper lower

--- upper_lower.py
+++ upper_lower.py
@@ -1,7 +1,8 @@
-
-
+def actual_printing(word):
+    print(word.upper())
+    print(word.lower())
 def main():
-    pass
-
+    word = input("type in a sentence.")
+    actual_printing(word)
 if __name__ == '__main__':
     main()
