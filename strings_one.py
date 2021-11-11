5da837cba4e510a0e16ab439351373558b0a0546
l1dl1d <javeekh@ssfs.org>
2021-11-11 11:55:56 -0500

    finished no first and last

--- strings_one.py
+++ strings_one.py
@@ -1,12 +1,13 @@
 def half_slice(word):
     mid = len(word)//2
-    half_one = str(print(word[mid:]))
-    half_two = str(print(word[0:mid]))
+    half_two = word[mid:]
+    half_one = word[0:mid]
     slice = half_two + half_one
+    return slice
 def no_first_last(str):
-    pass
-
-
+    length = len(str)
+    str_one = str[1:-1]
+    return str_one
 def longest(phrase):
     pass
 
@@ -14,7 +15,9 @@ def longest(phrase):
 def title_case(sentence):
     pass
 def main():
+    str = input("type in a word")
     word = input("type in a word")
-    half_slice(word)
+    print(half_slice(word))
+    print(no_first_last(str))
 if __name__ == '__main__':
     main()
