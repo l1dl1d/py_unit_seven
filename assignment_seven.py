c1d31a9dae48394b5d1cd0dc1786601868f5114b
l1dl1d <javeekh@ssfs.org>
2021-11-15 14:07:32 -0500

    assignment seven

--- assignment_seven.py
+++ assignment_seven.py
@@ -1,1 +1,9 @@
-
+def encode(phrase, shift):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    x = ""
+    for letter in phrase:
+        value = alphabet.index(letter)
+        value = value + shift
+        shifted_alphabet = alphabet[value]
+        x = x + shifted_alphabet
+    return x
