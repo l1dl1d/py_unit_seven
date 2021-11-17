316fa0b79a2614d5c4ee0778a82974fafa1994a4
kjaveed <javeekh@ssfs.org>
2021-11-16 21:16:27 -0500

    worked on assignment 7

--- assignment_seven.py
+++ assignment_seven.py
@@ -7,3 +7,24 @@ def encode(phrase, shift):
         shifted_alphabet = alphabet[value]
         x = x + shifted_alphabet
     return x
+def decode(phrase, shift):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    x = ""
+    for letter in phrase:
+        value = alphabet.index(letter)
+        value = value + shift
+        shifted_alphabet = alphabet[value]
+        x = x + shifted_alphabet
+    return x
+def main():
+    choice = input("press e, to encode, d to decode, or q to quit.")
+    if choice == e:
+        phrase = input("please enter text to be encoded:")
+        return encode()
+    elif choice == d:
+
+        return decode()
+    elif choice == q:
+        quit()
+if __name__ == '__main__':
+    main()
